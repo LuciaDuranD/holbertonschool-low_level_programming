6ff1a3979c726b516c223493046030382f18cecd
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-02-21 12:16:37 -0800

    C program that prints exactly and the artwork is useful followed by a new line, up to standard error.

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Principal function
+* Description: Print the text
+* Return: 1
+*/
+int main(void)
+{
+char *word="and that piece of art is useful\n" - "Dora Korpar, 2015-10-19\n"
+while (*word)
+putchat (*word++)
+return (1);
+}
