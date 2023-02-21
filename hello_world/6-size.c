d38017f2db117a98d785f15edd624d25a7203515
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-02-21 12:03:28 -0800

    print the size of various types on the computer on which it is compiled and run.

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,2 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - prints the byte size
+* Description: byte size
+* Return: 0
+*/
+int main(void)
+{
+int a;
+long int s;
+long long int d;
+char f;
+float g;
 
+printf("Size of a char: %1d byte(s)\n", sizeof(f));
+printf("Size of an int: %1d byte(s)\n", sizeof(a));
+printf("Size of a long int: %1d byte(s)\n", sizeof(s));
+printf("Size of a long long int: %1d byte(s)\n", sizeof(d));
+printf("Size of a float: %1d byte(s)\n", sizeof(g));
+return (0);
+}
