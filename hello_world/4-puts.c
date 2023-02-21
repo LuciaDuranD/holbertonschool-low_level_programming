c973252355bb1b71f22cb16043efb006d7eacc9b
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-02-21 11:05:03 -0800

    Write a C program that prints text followed by a new line.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: This function prints "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
