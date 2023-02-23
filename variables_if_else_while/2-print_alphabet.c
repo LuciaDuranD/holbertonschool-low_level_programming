6f298b657df7b234adea4d160815cb566d31ee7f
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-02-23 12:43:45 -0800

    Write a program that prints the alphabet in lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-  char a;
-  for (a = 'a'; a <= 'z' ; a++)
-  putchar(a);
-  putchar('\n');
-  return (0);
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
 }
