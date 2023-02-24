d2548443a1913d2be618aedabfeac31201923519
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-02-24 11:55:19 -0800

    the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - main fuction
+ *
+ * @n: the absolute value of an integer
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
